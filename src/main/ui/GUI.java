9cf484140c03ab990c13457a6353a010d97dac16
justintang97 <justinn.tangg@gmail.com>
2023-03-28 15:59:12 -0700

    fixed checkstyle in GUI

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -54,6 +54,7 @@ public class GUI extends JFrame {
         });
     }
 
+    // test
     public void loadButton() {
         buttonLoad.addActionListener(new ActionListener() {
             @Override
